de17199e5c8c1fb28f0d3374529a20005ba62970
Nitish Sinha <76836759+NITSlN@users.noreply.github.com>
2021-09-24 15:08:43 +0530

    Update forecast.js

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,14 +3,12 @@ const request = require('request');
 
 const forecast = (lon,lat,callback)=>{
     const url = 'http://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lon+'&appid=08ba5cef09681a49743441ba39d8585b&units=metric'
-    // const url = 'http://api.weatherstack.com/current?access_key=b42f18407a8c9f5dda843c8971c769e7&query='+lat+','+long
     request({ url, json: true }, (err, {body}) => {
     if (err) {
         callback('Unable to connect to weather services.',undefined)
     } else if (body.error) {
         callback('Unable to find location.',undefined);
     } else {
-        // callback(undefined,'Condition: ' + body.weather[0].description + ".");
         callback(undefined,{
             location:body.name,
             condition:body.weather[0].description,
@@ -18,13 +16,9 @@ const forecast = (lon,lat,callback)=>{
             feelsLike:body.main.feels_like,
             imgCode:body.weather[0].icon
         });
-        // callback(undefined,'The current temperature is '+body.main.temp+' degree celcius but the temperature felt like ' + body.main.feels_like + '.');
     }
 })
 }
 
-// forecast(-75.7088,44.1545,(err,data)=>{
-//     console.log(data);
-// })
 module.exports = forecast;
 
